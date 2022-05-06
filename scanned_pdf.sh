ea22c4b81bd09ef4118d10a895549d5c3c6e80ec
Oleksii Pavlenko <oleksii.pavlenko@ringteam.com>
2022-05-06 23:03:05 +0300

    Added stamp & background mix

--- scanned_pdf.sh
+++ scanned_pdf.sh
@@ -2,8 +2,28 @@
 #Idea from the post in HN. Make a Gist with the command code.
 #https://gist.github.com/jduckles/29a7c5b0b8f91530af5ca3c22b897e10
 INPUT_FILE="${1}"
+
+if [ ! -z "${2}" ]
+then
+    SIGN_FILE="${2}"
+else
+    SIGN_FILE="sign.jpg"
+fi
+
+if [ ! -z "${3}" ]
+then
+    BACKGR_FILE="${3}"
+else
+    BACKGR_FILE="background.jpg"
+fi
+
 xDIR="$(dirname "${INPUT_FILE}")"
 xNAME="$(basename -s .pdf "${INPUT_FILE}")"
-convert -density 150 "${INPUT_FILE}" -linear-stretch 3.5%x10% -blur 0x0.5 -attenuate 0.25 +noise Gaussian  -rotate $(seq -0.3 .01 0.3 | shuf | head -n1) aux_output.pdf
-gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite -sColorConversionStrategy=LeaveColorUnchanged -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dDownsampleMonoImages=true -dDownsampleGrayImages=true -dDownsampleColorImages=true -sOutputFile="${xDIR}/${xNAME}scanned.pdf" aux_output.pdf
-rm aux_output.pdf
+convert -density 150 "${INPUT_FILE}" -gravity center -extent 1275x1754 tmp.png
+convert tmp.png -gravity center -crop 1240x1754+0+0 +repage tmp.png
+convert tmp.png \( "${SIGN_FILE}" -rotate $(seq -1.5 .01 1.5 | shuf | head -n1) \) -gravity Center -geometry 200x200-180+530 -composite tmp.png
+composite -dissolve 3 -gravity Center "${BACKGR_FILE}" tmp.png -alpha Set tmp.png
+convert tmp.png -linear-stretch 3.5%x10% -blur 0x0.5 -attenuate 0.25 +noise Gaussian  -rotate $(seq -0.3 .01 0.3 | shuf | head -n1) aux_output.pdf
+gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite -sColorConversionStrategy=LeaveColorUnchanged -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dDownsampleMonoImages=true -dDownsampleGrayImages=true -dDownsampleColorImages=true -sOutputFile="${xDIR}/${xNAME}_scanned.pdf" aux_output.pdf
+
+rm tmp.png aux_output.pdf
